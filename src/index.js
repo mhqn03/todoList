cb2f56411aef4f64cf9e86641dd621128a1efa9b
Minh Quân <mquanv12@gmail.com>
2023-05-10 19:44:14 +0700

    ok

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
+
+reportWebVitals();
